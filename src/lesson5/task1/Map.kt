0be1ea7dd792eb4983eb70474d21023ad43e540f
GreenBeaver <leonkhvatskin@gmail.com>
2018-10-25 23:50:25 +0300

    lesson 5 just started

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -2,6 +2,8 @@
 
 package lesson5.task1
 
+import lesson4.task1.mean
+
 /**
  * Пример
  *
@@ -122,15 +124,7 @@ fun buildGrades(grades: Map<String, Int>) =
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
-    var answer = true
-    for ((key) in a)
-        if (key !in b || b[key] != a[key]) {
-            answer = false
-            break
-        }
-    return answer
-}
+fun containsIn(a: Map<String, String>, b: Map<String, String>) = b.plus(a) == b
 
 /**
  * Средняя
@@ -142,7 +136,8 @@ fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>) =
+        stockPrices.groupBy ({ it.first }, { it.second }).mapValues { mean(it.value) }
 
 /**
  * Средняя
