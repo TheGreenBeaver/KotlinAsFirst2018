62f6e4f3f2966d86ba9b15cb44a87fb39db41abb
GreenBeaver <leonkhvatskin@gmail.com>
2018-11-20 02:18:44 +0300

    lesson 7 started

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -135,7 +135,7 @@ class Tests {
         assertEquals(694, fromRoman("DCXCIV"))
         assertEquals(49, fromRoman("XLIX"))
         assertEquals(-1, fromRoman("Z"))
-        assertEquals(-1, fromRoman("CMD"))
+        assertEquals(-1, fromRoman("MCMD"))
     }
 
     @Test
